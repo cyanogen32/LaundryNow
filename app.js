8efadded246c0528bb17b1672e9bff8c1c9d36b7
cyanogen32 <ddluc32@gmail.com>
2013-11-11 18:02:09 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+
+app.use(express.cookieParser());
+app.use(express.session({secret: '1234567890QWERTY'}));
+
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+
+//Arduino Routes
+app.get('/recieve_pulse', routes.recieve_pulse); 
+
+
+//User Routes
+app.post('/initialize', routes.initialize); 
+app.get('/set_arduino', routes.set_arduino);
+app.get('/check_status', routes.check_status);  
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
